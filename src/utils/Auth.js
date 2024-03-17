51b8b19be6473931bc3ce7eac0e8c5f57daac1ec
Evandro <34420396+ev4ndropc@users.noreply.github.com>
2024-03-17 03:34:16 -0300

    Update Auth.js

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -5,20 +5,20 @@ export default (request, response) => {
     const authHeader = request.headers.authorization
 
     if (!authHeader)
-      return response.status(401).json({ error: true, message: 'No token provided!', notallowed: true })
+      return response.status(401).json({ ok: false, message: 'No token provided!', notallowed: true })
 
     const parts = authHeader.split(' ')
 
     if (!parts.length === 2)
-      return response.status(401).json({ error: true, message: 'Token error!', notallowed: true })
+      return response.status(401).json({ ok: false, message: 'Token error!', notallowed: true })
 
     const [scheme, token] = parts
 
     if (!scheme === 'Bearer')
-      return response.status(401).json({ error: true, message: 'Token malformatted!', notallowed: true })
+      return response.status(401).json({ ok: false, message: 'Token malformatted!', notallowed: true })
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) return response.status(401).json({ error: true, messsage: 'Token invalid!', notallowed: true })
+      if (err) return response.status(401).json({ ok: false, messsage: 'Token invalid!', notallowed: true })
 
       request._id = decoded._id
       request.email = decoded.email
@@ -26,7 +26,6 @@ export default (request, response) => {
 
     return { ok: true }
   } catch (error) {
-    console.log(error)
     return { ok: false }
   }
 
