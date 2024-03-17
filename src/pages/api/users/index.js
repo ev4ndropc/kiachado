9fe279ba824f708df607f1e55364915b2da2e48c
Evandro <34420396+ev4ndropc@users.noreply.github.com>
2024-03-17 03:04:11 -0300

    Update index.js

--- src/pages/api/users/index.js
+++ src/pages/api/users/index.js
@@ -1,9 +1,18 @@
 import Auth from "../../../utils/Auth"
 
 export default function Login(request, response) {
-    const isValid = Auth(request, response)
-    if(!isValid.ok)
-    return response.status(400).json({ ok: false })
-    
-    return response.status(200).json({ ok: true })
+    try {
+        const isValid = Auth(request, response)
+
+        if (!isValid.ok)
+            return response.status(400).json({ ok: false })
+
+
+        return response.status(200).json({ ok: true })
+    } catch (error) {
+        console.log(error)
+        return response.status(400).json({ ok: false })
+
+    }
+
 }
